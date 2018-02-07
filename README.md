7c190d29ddb702a9dcf5e0d39f44b61e4b5a2fd9
Abdallah Samman <sammanabdallah@gmail.com>
2018-02-07 12:14:56 +0200

    add support to international amazon websites

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 https://chrome.google.com/webstore/detail/1-click-amazon-books-to-g/ncpijjhmmggjjbhpkcgehplocpnibehj?ref=producthunt
+### Screenshots:
+![screenshot1](https://ph-files.imgix.net/08c9ad80-0d1c-4c16-9810-2aa0c14967e6?auto=format&auto=compress&codec=mozjpeg&cs=strip)
+![screenshot2](https://ph-files.imgix.net/05edf36f-bb73-4424-b5c7-6430ebcece22?auto=format&auto=compress&codec=mozjpeg&cs=strip)
+
 
 ### TODO:
 [ ] Make bookmarklet to go to goodreads from amazon book page
--- content.js
+++ content.js
@@ -20,20 +20,25 @@ function addGoodreadsBtn(){
   var goodreads_link = makeGoodreadsLink( getISBN13fromLink() );
   var avg_custom_reviews = document.querySelector('#averageCustomerReviews');
   if (avg_custom_reviews !== null){
-    avg_custom_reviews.innerHTML += `<br><br><a href="${goodreads_link}">See book on <img src="${chrome.runtime.getURL('goodreads_logo.svg')}" height="20px"/></a>`;
+    avg_custom_reviews.innerHTML += `<br><br><a href="${goodreads_link}">See book on <img src="${chrome.runtime.getURL('goodreads_logo.svg')}" height="20px"/></a><br><br>`;
     return;
   }
 
   var book_formats = document.querySelector('#formats');
   if (book_formats !== null){
-    book_formats.innerHTML += `<br><br><a href="${goodreads_link}">See book on <img src="${chrome.runtime.getURL('goodreads_logo.svg')}" height="20px"/></a>`;
+    book_formats.innerHTML += `<br><br><a href="${goodreads_link}">See book on <img src="${chrome.runtime.getURL('goodreads_logo.svg')}" height="20px"/></a><br><br>`;
     return;
   }
 }
 
-var is_book_check_1 =  getElementByXpath('//*[@id="nav-subnav"]/a[1]/span') !== null && getElementByXpath('//*[@id="nav-subnav"]/a[1]/span').innerText === "Books"
-var is_book_check_2 = document.querySelector('#nav-subnav[data-category=books]') !== null
-
-if(is_book_check_1 || is_book_check_2){
+var is_book_check_1 = document.body.innerHTML.indexOf('ISBN-10') !== -1;
+if(is_book_check_1){
   addGoodreadsBtn();
+}else{
+  var is_book_check_2 = getElementByXpath('//*[@id="nav-subnav"]/a[1]/span') !== null && getElementByXpath('//*[@id="nav-subnav"]/a[1]/span').innerText === "Books";
+  var is_book_check_3 = document.querySelector('#nav-subnav[data-category=books]') !== null;
+  var is_book_check_4 = document.querySelector('#nav-subnav[data-category=digital-text]') !== null;
+  if(is_book_check_2 || is_book_check_3 || is_book_check_4){
+    addGoodreadsBtn();
+  }
 }
